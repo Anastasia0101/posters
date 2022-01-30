252cfbd4011cb0d367722a2e5e8be42b034e9c6d
Anastasia <anastasia.storozhenko@masterofcode.com>
2022-01-30 12:09:07 +0200

    added search by title

--- src/app/components/posters-list/posters-list.component.ts
+++ src/app/components/posters-list/posters-list.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Poster } from 'src/app/models/poster.model';
 import { PostersService } from 'src/app/services/posters.service';
 import { MatDialog } from '@angular/material/dialog';
 import { PosterDialogComponent } from '../poster-dialog/poster-dialog.component';
+import { combineLatest, fromEvent, Observable } from 'rxjs';
+import { map, startWith, withLatestFrom } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-posters-list',
@@ -10,7 +13,10 @@ import { PosterDialogComponent } from '../poster-dialog/poster-dialog.component'
   styleUrls: ['./posters-list.component.css']
 })
 export class PostersListComponent implements OnInit {
+  posters$: Observable<Poster[]>;
   posters: Poster[];
+  @ViewChild('seacher', {static: false}) inputRef: ElementRef<HTMLInputElement>;
+  control = new FormControl('');
 
   constructor(
     private postersService: PostersService,
@@ -18,13 +24,21 @@ export class PostersListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.getPosters();
+    this.posters$ = this.postersService.getPosters();
+    this.posters$.subscribe((posters: Poster[]) => {
+      this.posters = posters;
+    })
   }
 
-  getPosters(): void {
-    this.postersService.getPosters().subscribe((posters: Poster[]) => {
-      this.posters = posters;
-    });
+  ngAfterViewInit(): void {
+    const inputedText = fromEvent(this.inputRef.nativeElement, 'input').pipe(
+      withLatestFrom(this.control.valueChanges.pipe(startWith(''))),
+      map((data) => data[1]),
+    );
+    combineLatest([ this.posters$, inputedText ]).pipe(
+      map(([posts, text]) => posts.filter(item => item.title.includes(text))),
+    )
+    .subscribe((posters: Poster[]) => this.posters = posters);
   }
 
   createPoster(): void {
